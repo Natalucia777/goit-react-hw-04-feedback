46428f5774a5aa8d1c430acd6c65fe96f57679ca
Natalya Demidenko <natalucia777@ukr.net>
2023-07-16 23:02:09 +0300

    hw-react-4-1-fb--4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  
   const onFeedback = state => {
     switch (state) {
       case 'good':
@@ -34,7 +33,7 @@ function App() {
     return Math.round((good / countTotalFeedback()) * 100);
   }
 
-    const options = Object.keys({good, neutral, bad });
+  const options = Object.keys({ good, neutral, bad });
     return (
       <Container>
       <div>
